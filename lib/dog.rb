73a73b07cc6088167d70972931742073fd85856f
Donna Amos <donnacamos.developer@gmail.com>
2019-03-02 17:36:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,23 @@
-class Dog 
-  def initialize(fido, pug=mutt)
+class Dog
+  def initialize (fido, pug = "Mutt")
     @name = fido
-    @breed = pug
+    @breed = pug 
   end
-  def name=(fido)
-    @name = fido 
-  end 
+
+  def name=(fido, pug = "Mutt")
+    @name = fido
+    @breed = pug 
+  end
+
   def name
     @name
-  end 
-  
-  def breed=(pug=mutt) 
-    @breed = pug
   end
   def breed
     @breed
-  end
- end 
+  end 
+end 
+  
+  fido = Dog.new("Fido")
  
  
  
